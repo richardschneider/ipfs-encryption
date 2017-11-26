7c83bdc16fd9e07e1084f9e75264d3bb8ffdbdbc
Richard Schneider <makaretu@gmail.com>
2017-11-26 19:29:48 +1300

    feat: all derived encryption key parameters to be specified

--- src/keystore.js
+++ src/keystore.js
@@ -3,6 +3,7 @@
 const mkdirp = require('mkdirp')
 const sanitize = require("sanitize-filename")
 const forge = require('node-forge')
+const deepmerge = require('deepmerge')
 const crypto = require('crypto')
 const path = require('path')
 const fs = require('fs')
@@ -10,7 +11,15 @@ const fs = require('fs')
 const keyExtension = '.pem'
 
 const defaultOptions = {
-  createIfNeeded: true
+  createIfNeeded: true,
+
+  //See https://cryptosense.com/parameter-choice-for-pbkdf2/
+  dek: {
+    keyLength: 512 / 8,
+    iterationCount: 10000,
+    salt: 'you should override this value with a crypo secure random number',
+    hash: 'sha512'
+  }
 }
 
 function validateKeyName (name) {
@@ -21,20 +30,35 @@ function validateKeyName (name) {
 
 class Keystore {
   constructor (options) {
-    const opts = Object.assign({}, defaultOptions, options)
+    const opts = deepmerge(defaultOptions, options)
     
     if (opts.createIfNeeded) {
       mkdirp.sync(opts.store)
     }
 
+    // Enfore NIST SP 800-132
+    const minKeyLength = 112 / 8
+    const minSaltLength = 128 / 8
+    const minIterationCount = 1000
     if (!opts.passPhrase || opts.passPhrase.length < 20) {
-      throw new Error('passPhrase is required of at least 20 characters')
+      throw new Error('passPhrase must be least 20 characters')
     }
-    //See https://cryptosense.com/parameter-choice-for-pbkdf2/
-    const klen = 512 / 8
-    const icount = 10000
-    const salt = 'xyzzy'
-    let dek = forge.pkcs5.pbkdf2(opts.passPhrase, salt, icount, klen, 'sha512')
+    if (opts.dek.keyLength < minKeyLength) {
+      throw new Error(`dek.keyLength must be least ${minKeyLength} bytes`)
+    }
+    if (opts.dek.salt.length < minSaltLength) {
+      throw new Error(`dek.saltLength must be least ${minSaltLength} bytes`)
+    }
+    if (opts.dek.iterationCount < minIterationCount) {
+      throw new Error(`dek.iterationCount must be least ${minIterationCount}`)
+    }
+
+    let dek = forge.pkcs5.pbkdf2(
+      opts.passPhrase,
+      opts.dek.salt,
+      opts.dek.iterationCount,
+      opts.dek.keyLength,
+      opts.dek.hash)
     dek = forge.util.bytesToHex(dek)
 
     this.store = opts.store
